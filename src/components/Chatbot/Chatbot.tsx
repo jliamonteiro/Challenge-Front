028880ad4775298f3f9b063c801de6dc56b6ea06
Victor-Estrella <victorhcarracci@gmail.com>
2024-11-04 09:35:56 -0300

    Tentando fazer chatbot ibm

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -1,23 +1,33 @@
 "use client"
 import { useEffect } from 'react';
 
+interface WatsonAssistantOptions {
+  integrationID: string;
+  region: string;
+  serviceInstanceID: string;
+  onLoad: (instance: { render: () => Promise<void> }) => Promise<void>;
+  clientVersion?: string; // Tornar clientVersion opcional
+}
+
+declare global {
+  interface Window {
+    watsonAssistantChatOptions?: WatsonAssistantOptions;
+  }
+}
+
 export default function Chatbot() {
   useEffect(() => {
     // Definindo as opções do Watson Assistant
-    (window as any).watsonAssistantChatOptions = {
-      integrationID: "b282140d-9ec1-4e31-b233-356034ce8636", // ID da integração
-      region: "us-south", // Região da integração
-      serviceInstanceID: "4895944c-12f0-4b43-9a58-2390e3fc6fd0", // ID da instância do serviço
-      onLoad: async (instance: any) => {
-        await instance.render();
-      },
+    window.watsonAssistantChatOptions = {
+      integrationID: "b282140d-9ec1-4e31-b233-356034ce8636",
+      region: "us-south",
+      serviceInstanceID: "4895944c-12f0-4b43-9a58-2390e3fc6fd0",
+      onLoad: async (instance) => { await instance.render(); }
     };
 
     // Função para carregar o script do Watson Assistant
     const script = document.createElement('script');
-    script.src = `https://web-chat.global.assistant.watson.appdomain.cloud/versions/${
-      (window as any).watsonAssistantChatOptions.clientVersion || 'latest'
-    }/WatsonAssistantChatEntry.js`;
+    script.src = `https://web-chat.global.assistant.watson.appdomain.cloud/versions/${window.watsonAssistantChatOptions.clientVersion || 'latest'}/WatsonAssistantChatEntry.js`;
     script.async = true;
     document.head.appendChild(script);
 
